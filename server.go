75c0b3b5de9978adc030efd41e2eea828243553b
forestdepo <sulatn4ik@kubsu.ru>
2026-03-10 10:46:37 +0300

    parse a form

--- server.go
+++ server.go
@@ -5,12 +5,43 @@ import (
 	"net/http"
 )
 
-func myHandler(output http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/"):]
-	fmt.Fprintf(output, "<h6>%s</h6>", title)
+type Person struct {
+	fullName string
+	phone string
+	email string
+	gender int8
+	birthDate string
+	languages []string
+	bio string
+	contract bool
+}
+
+func indexHandler(output http.ResponseWriter, request *http.Request){
+	http.ServeFile(output, request, "index.html")
+}
+
+func parseForm(output http.ResponseWriter, request *http.Request){
+	if request.Method != "Post" {
+		http.Redirect(output, request, "/", http.StatusSeeOther)
+	}
+
+		fullName := request.FormValue("fullName") // first only
+
+		phone := request.FormValue("phone")
+		email := request.FormValue("email")
+		gender := request.FormValue("gender")
+		birthDate := request.FormValue("birthDate")
+		languages := request.Form["languages"] // all!
+		bio := request.FormValue("bio")
+		contract := request.FormValue("contract")
+
+		fmt.Println(
+			fullName,"\n",phone, "\n", email, "\n", gender, "\n", birthDate, "\n", 
+			languages, "\n",  bio, "\n", contract)
 }
 
 func main(){
-	http.HandleFunc("/", myHandler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/submit", parseForm)
 	http.ListenAndServe(":8080", nil)
 }
